42a5373161f7a4120785c677598e9513a2b33d38
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-24 00:11:29 +0700

    [global-function] add: function to get and compare all dob with injectionage

--- utils/util-function.js
+++ utils/util-function.js
@@ -1,5 +1,7 @@
 const moment = require("moment");
 const diaryModel = require("../models/diary.model");
+const inoculateModel = require("../models/inoculate.model");
+const userModel = require("../models/user.model");
 
 function monthDiff(d1, d2) {
   var months;
@@ -11,15 +13,41 @@ function monthDiff(d1, d2) {
 
 module.exports = {
   sendNotificationMail: async function () {
-    console.log("hehe");
+    console.log("begin");
     //create variable to keep today's date
     const currentDate = new Date();
 
     //get all diary's dob to compare
     const diaries = await diaryModel.getAllToSendMail();
 
-    //calculate age of each diary (month)
+    //get milestones of incjection from db to compare with diary's dob
+    const singleAges = await inoculateModel.getAllInjectionAgeWithoutLoopSpan();
+    const loopAges = await inoculateModel.getAllLoopSpanInjection();
 
-    console.log(diaries);
+    //calculate age of each diary (month)
+    diaries.forEach(async (element) => {
+      let totalMonthAge = monthDiff(new Date(element.dob), currentDate);
+
+      // console.log(totalMonthAge);
+
+      //check for injection without looping time
+      if (
+        singleAges.findIndex(
+          (element) => parseInt(element.injectionAge) === totalMonthAge
+        ) === -1
+      ) {
+      } else {
+        //get email of diary's owner
+        let userEmail = await userModel.getEmailById(element.id_user);
+        //get all vaccine need to be injected
+        let listVaccineToInject = await inoculateModel.getAllVaccineByInjectionAge(
+          totalMonthAge
+        );
+        //call function to send email
+      }
+    });
+
+    //console.log(diaries);
+    console.log("end");
   },
 };
