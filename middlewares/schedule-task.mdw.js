e08576c03420ea74903e3745afb307b4875efc15
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-04-12 20:53:53 +0700

    [schedule-mdw]: add arrayTask, method to stop & destroy task in arrayTask

--- middlewares/schedule-task.mdw.js
+++ middlewares/schedule-task.mdw.js
@@ -4,7 +4,10 @@ const globalFunction = require("../utils/util-function");
 let task;
 
 module.exports = scheduleTask = {
+  arrayTask: [],
+
   autoMailState: true,
+
   startAutoMailing: async function () {
     task = cron.schedule(
       "0 6 * * *",
@@ -17,5 +20,18 @@ module.exports = scheduleTask = {
 
   stopAutoMailing: async function () {
     task.stop();
+  },
+
+  stopTaskInArray: async function (index) {
+    if (index > -1 && index < this.arrayTask.length) {
+      this.arrayTask[index].stop();
+    }
+  },
+
+  destroyTaskInArray: async function (index) {
+    if (index > -1 && index < this.arrayTask.length) {
+      this.arrayTask[index].destroy();
+      console.log(this.arrayTask[index]);
+    }
   },
 };
