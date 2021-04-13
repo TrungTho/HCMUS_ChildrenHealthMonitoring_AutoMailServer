46ba4ab87f17b608c110d641ce7ed886fdac3a6c
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-04-14 00:36:52 +0700

    [schedule-mdw]: (NOT tested yet) add auto schedule method to reset array task at 00:01 a.m gtm+7

--- middlewares/schedule-task.mdw.js
+++ middlewares/schedule-task.mdw.js
@@ -6,9 +6,10 @@ let task;
 module.exports = scheduleTask = {
   autoMailState: true,
 
+  //some things this server will work by it owns
   startAutoMailing: async function () {
     task = cron.schedule(
-      "0 6 * * *",
+      "0 6 * * *", //6:00 am
       () => {
         globalFunction.sendNotificationMail();
       },
@@ -16,6 +17,17 @@ module.exports = scheduleTask = {
     );
   },
 
+  scheduleResetTaskArray: async function () {
+    cron.schedule(
+      "1 0 * * *", //00:01 am
+      () => {
+        this.resetArrayTask();
+      },
+      { scheduled: true, timezone: "Asia/Bangkok" }
+    );
+  },
+
+  //-----------------------------------------
   stopAutoMailing: async function () {
     task.stop();
   },
@@ -37,7 +49,7 @@ module.exports = scheduleTask = {
     }
   },
 
-  resetArrayTask: async function (index) {
+  resetArrayTask: async function () {
     for (task of this.arrayTask) {
       task.task.stop();
       task.task.destroy();
